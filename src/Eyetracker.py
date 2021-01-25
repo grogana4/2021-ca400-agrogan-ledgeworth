9e89a0746a61c838ca1f0c1d2ff66f96747989ad
Abe Grogan <abe.grogan4@mail.dcu.ie>
2021-01-25 16:06:07 +0000

    progress from 18th - 25th Jan:\n - added face tracking to live video feed (may require tweaking) \n- tested with a Queued video feed for higher FPS while tracking \n- currently working on eye tracking relative to display

---
+++ src/Eyetracker.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:24d49ee72bcc7f97ff6b8be4a5b58367f82813fc6a2a49073b23c86163ab47d7
+size 4231
---
+++ src/VideoCapture.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ee25be0d48cf7973854cac923741c631fdd8b14e0f2b18aebe74e24d5ca0dc77
+size 1431
