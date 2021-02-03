ea3155eb01843a5171536f4dd43abe11d09a9782
Luke Edgeworth <luke.edgeworth2@mail.dcu.ie>
2021-02-03 17:09:33 +0000

    progress from 26th jan -3rd feb - added comments to better explain code -added eye masking and pupil tracking -now working on track world coordinates to screen coordinates

--- src/Eyetracker.py
+++ src/Eyetracker.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:24d49ee72bcc7f97ff6b8be4a5b58367f82813fc6a2a49073b23c86163ab47d7
-size 4231
+oid sha256:bd3de452505e76828796ca6a22f3b5a4e192d382c65a35dd85d3dfd3cb75bd04
+size 12614
