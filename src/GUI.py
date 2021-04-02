1679791c0d7c70307f0101a9891481e55eb8846d
edgewol2 <luke.edgeworth2@mail.dcu.ie>
2021-04-02 17:36:19 +0100

    Buggy threading implemented

--- src/GUI.py
+++ src/GUI.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0265a886a6d57686e696437b057d9dbf3edf63745813db5bf5191ff37f123873
-size 34810
+oid sha256:f6620aee5887fc7cff2bab458c68870c0034e3b57b63c931a6437cec31c7d825
+size 34640
