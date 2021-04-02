c832dec00c2d375b58efad8262baf2dbe5a2cfb9
edgewol2 <luke.edgeworth2@mail.dcu.ie>
2021-04-02 08:58:42 +0100

    Gesture settings set up

--- src/GUI.py
+++ src/GUI.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8da190422c04b41bdf23ec4bc4b6c93dca7846d9ab8309494d90a0379e8ed85b
-size 27403
+oid sha256:974a1d82f7faa94c8aab0c7098a0e6ee07618c5672e9fb2197877154f8784678
+size 36446
