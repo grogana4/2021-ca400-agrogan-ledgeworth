94eb5abbfb92dc0cac4c93387eddd04a02176d84
edgewol2 <luke.edgeworth2@mail.dcu.ie>
2021-04-09 14:45:50 +0100

    Got threaded cameras working on both frames

--- src/GUI.py
+++ src/GUI.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bef238d148895114b078b654dc945bccc9f62a5eeedaec5cc16a22bc12bbd29a
-size 34688
+oid sha256:230e1b7aeae524592551fdcb66ed4381f53b2f1c8925ceee24deab66248fcbd6
+size 31641
