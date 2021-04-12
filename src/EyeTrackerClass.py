0c8efb37e0dc77785ac66992dfc50811d01c3fb2
edgewol2 <luke.edgeworth2@mail.dcu.ie>
2021-04-12 15:58:22 +0100

    fixed few errors

--- src/EyeTrackerClass.py
+++ src/EyeTrackerClass.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:577a186e75662f5c16fc82109bc7107305250294f0fba1c6ed27c6c7d8ce16d6
-size 12149
+oid sha256:588fe62f8a4810ade2e91115ec642f2fce3560922334cbabb27472a855b40fec
+size 12383
