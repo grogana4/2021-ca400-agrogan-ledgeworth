91d94c202ba44e4140743a5f992d6ebcb8e2eefd
edgewol2 <luke.edgeworth2@mail.dcu.ie>
2021-05-04 19:05:29 +0100

    More integration tests

--- src/test_GUI.py
+++ src/test_GUI.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff31204924c4eb7f23743cc6fb68ef6b2af16acb4ab3e78e0638a7b012626960
-size 5429
+oid sha256:26490658423a36c447cad674e281ed5f43c126fd7e4a9d42f5a8f60928fe23ea
+size 6460
